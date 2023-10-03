5c7f1dd239912ff9beb291a802dec0e4d3a15900
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-04 00:03:14 +0300

    :safety_vest: Added validation for update & create product schema

--- products/schemas.py
+++ products/schemas.py
@@ -1,6 +1,7 @@
 import datetime
+import re
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class ProductBase(BaseModel):
@@ -16,8 +17,79 @@ class ProductBase(BaseModel):
         }
 
 
-class CreateProduct(ProductBase):
-    pass
+class CreateProduct(BaseModel):
+    name: str
+    description: str
+    price: float
+
+    @field_validator("name")
+    def validate_name(cls, name):
+        pattern = r"^[A-Za-z][A-Za-z0-9\s]*$"
+
+        if not re.match(pattern, name):
+            raise ValueError(
+                "Name must only contain letters, "
+                "numbers, or spaces"
+            )
+
+        return name
+
+    @field_validator("description")
+    def validate_description(cls, description):
+        pattern = r"^[A-Za-z][A-Za-z0-9\s]*$"
+
+        if not re.match(pattern, description):
+            raise ValueError(
+                "Description must only contain letters, "
+                "numbers, or spaces"
+            )
+
+        return description
+
+    @field_validator("price")
+    def validate_price(cls, price):
+        if float(price) < 1:
+            raise ValueError("Price must be greater than 0")
+        return price
+
+
+class UpdateProduct(BaseModel):
+    name: str = None
+    description: str = None
+    price: float = None
+
+    @field_validator("name")
+    def validate_name(cls, name):
+        if name is not None:
+            pattern = r"^[A-Za-z][A-Za-z0-9\s]*$"
+
+            if not re.match(pattern, name):
+                raise ValueError(
+                    "Name must only contain letters, "
+                    "numbers, or spaces"
+                )
+
+            return name
+
+    @field_validator("description")
+    def validate_description(cls, description):
+        if description is not None:
+            pattern = r"^[A-Za-z][A-Za-z0-9\s]*$"
+
+            if not re.match(pattern, description):
+                raise ValueError(
+                    "Description must only contain letters, "
+                    "numbers, or spaces"
+                )
+
+            return description
+
+    @field_validator("price")
+    def validate_price(cls, price):
+        if price is not None:
+            if float(price) < 1:
+                raise ValueError("Price must be greater than 0")
+            return price
 
 
 class Product(ProductBase):
