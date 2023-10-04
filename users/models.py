90fea7faea9178fc7233d97049f57b00995a2a9d
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-04 19:36:50 +0300

    :package: Added user model

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+)
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(
+        Integer, primary_key=True, index=True
+    )
+    username = Column(
+        String(255), unique=True, nullable=False
+    )
+    email = Column(
+        String(255), unique=True, nullable=False
+    )
+    phone_number = Column(
+        String(255), unique=True, nullable=False
+    )
+    password = Column(
+        String(255), nullable=False
+    )
+    is_admin = Column(
+        Boolean(), default=False
+    )
+    is_activated = Column(
+        Boolean(), default=False
+    )
