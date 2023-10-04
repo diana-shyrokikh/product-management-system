cd1f9dbe42399d2e4f601c7bb09cc7414f8ad9df
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-04 19:45:44 +0300

    :sparkles: Added user_router on main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from products import router as product_router
 from product_categories import router as category_router
+from users import router as user_router
 
 app = FastAPI()
 
 app.include_router(product_router.router)
 app.include_router(category_router.router)
+app.include_router(user_router.router)
 
 
 @app.get("/")
