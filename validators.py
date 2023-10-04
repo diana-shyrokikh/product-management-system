2626442d6e49ba6be21b1e9450c4e6603fb0087e
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-04 23:08:53 +0300

    :safety_vest: Added validate_number

--- validators.py
+++ validators.py
@@ -9,6 +9,16 @@ def validate_string(string, pattern, error_message):
         return string
 
 
+def validate_number(number, field_name):
+    if number:
+        if number < 1:
+            raise ValueError(
+                f"{field_name} must be greater than 0"
+            )
+
+        return number
+
+
 def validate_phone_number_format(phone_number):
     if phone_number:
         phone_number = phone_number.replace(" ", "").replace(
