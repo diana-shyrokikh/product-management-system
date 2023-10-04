0cb3dd639b3d3e29ad3e78b83bd83efe314bf4c8
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-04 21:51:26 +0300

    :safety_vest: Added validators.py

---
+++ validators.py
@@ -0,0 +1,25 @@
+import re
+
+
+def validate_string(string, pattern, error_message):
+    if string:
+        if not re.match(pattern, string.strip()):
+            raise ValueError(error_message)
+
+        return string
+
+
+def validate_phone_number_format(phone_number):
+    if phone_number:
+        phone_number = phone_number.replace(" ", "").replace(
+            "(", ""
+        ).replace(")", "").replace("+", "").replace("-", "")
+
+        if not phone_number.isdigit():
+            raise ValueError(
+                "Phone number can contain digits, "
+                "spaces, symbols: (, +, -, )"
+            )
+
+        return phone_number
+
