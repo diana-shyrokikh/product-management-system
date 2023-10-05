5a49fdadc2f0950227e08d93e9dd8259b06f7a4a
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-05 19:08:11 +0300

    :heavy_plus_sign: Added is_admin dependency function

--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,5 @@
-import jwt
-
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-
-from pydantic.v1 import ValidationError
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
@@ -12,11 +8,7 @@ from database import SessionLocal
 
 from users import schemas
 from users.crud import get_user_by_username
-from users.utils import (
-    ALGORITHM,
-    JWT_SECRET_KEY,
-    JWT_REFRESH_SECRET_KEY
-)
+from users.utils import verify_token
 
 reusable_oauth = OAuth2PasswordBearer(
     tokenUrl="/login",
@@ -38,33 +30,12 @@ async def common_object_parameters(
 
 async def get_current_user(
     token: str = Depends(reusable_oauth),
-    token_type: str = "access",
     db: AsyncSession = Depends(get_db)
 ) -> schemas.User:
-    secret_key = (
-        JWT_SECRET_KEY if token_type == "access"
-        else JWT_REFRESH_SECRET_KEY
+    payload = verify_token(
+        token=token,
+        token_type="access",
     )
-
-    try:
-        payload = jwt.decode(
-            token, secret_key, algorithms=[ALGORITHM]
-        )
-
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    except(jwt.InvalidSignatureError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
     user = await get_user_by_username(
         db,
         payload.get("sub")
@@ -74,6 +45,18 @@ async def get_current_user(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Could not find user",
+        )
+
+    return user
+
+
+def is_admin(
+    user: schemas.User = Depends(get_current_user)
+) -> schemas.User:
+    if not user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don't have permission to access this resource",
         )
 
     return user
